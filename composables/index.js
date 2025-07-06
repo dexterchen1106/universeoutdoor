18011e4c27db377ed85864a64ac83bbe92dc2b35
dexterchen1106 <dexterchen1106@outlook.com>
2025-07-06 15:37:52 +0800

    update

--- composables/index.js
+++ composables/index.js
@@ -1,8 +1,3 @@
-// Examples
-export const useFoo = () => {
-  return useState('foo', () => 'bar')
-}
-
-export const capitalize = (str) =>  {
-  return str.charAt(0).toUpperCase() + str.slice(1)
-}
+export const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
